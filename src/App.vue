8dde5bf065c37fcc098f3fa48760193a32f524c2
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-05-25 16:57:53 -0400

    Importantando componentes e css

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content">
+    <CadastrarCursos />
+    <br />
+    <ListaCursos />
+  </div>
+</template>
+
+<script>
+import CadastrarCursos from "./components/CadastrarCursos";
+import ListaCursos from "./components/ListaCursos";
+export default {
+  name: "App",
+  components: {
+    CadastrarCursos,
+    ListaCursos,
+  },
+};
+</script>
+
+<style>
+@import './css/cadastro.css';
+@import './css/busca-lista.css';
+</style>
